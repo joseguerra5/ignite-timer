356c05a52c539a0077d7a6a3567296f024cfba4f
“José <jose.luiz.guerra1@gmail.com>
2024-09-23 14:45:41 +0100

    initializing project

---
+++ src/App.tsx
@@ -0,0 +1,16 @@
+import { Button } from "./components/Button";
+
+
+export function App() {
+
+  return (
+    <>
+      <Button color="primary"></Button>
+      <Button color="secondary"></Button>
+      <Button color="danger"></Button>
+      <Button ></Button>
+    </>
+  )
+}
+
+
